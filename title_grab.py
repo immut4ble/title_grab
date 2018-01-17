fd3d87865f0fd546ddd7cf242c366240ac3265a2
packet <packet@localhost.localdomain>
2018-01-17 16:33:30 +1300

    fixing it up for linux
    Needed a bunch of little changes, notice the curl command doesn't take
    an arg now for some reason? it's just one string.. w/e

--- title_grab.py
+++ title_grab.py
@@ -4,14 +4,23 @@ import sys
 import subprocess
 
 def grab_title():
-    output = subprocess.check_output(['curl -L', sys.argv[1]], shell=True)
+    output = None
+    try:
+        output = subprocess.check_output(["curl -L -m8 " + sys.argv[1]], shell=True)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+
+    output = output.lower()
+    output = output.replace("\n", "")
     opening_title = output.find("<title>")
     closing_title = output.find("</title>")
     title = output[opening_title+7:closing_title]
     title = title.strip()
-    print(title)
+    if len(title) > 1:
+        print(sys.argv[1] + ": " + title)
 
 if len(sys.argv) != 2:
+    print(sys.argv)
     print("useage: title_grab ip-address")
 else:
     grab_title()
